5eef0bba87c5d45bb8e450d207d560408136adb4
Mary Jacobs <TheMaryJacobs@gmail.com>
2019-11-14 22:52:16 -0600

    application screenshots

--- README.md
+++ README.md
@@ -6,10 +6,13 @@ https://themaryjacobs.github.io/PasswordGenerator/
 
 
 ## Overview
-Create an application that can generate strong passwords quickly and effortlessly saves employees time and ensures secure access to data.
+An application that can generate strong passwords quickly and effortlessly that saves employees time and ensures secure access to data.
 
 ## Description
+Using a variety of functions and events,this application generates a unique password based on selected criteria. The password length must be between 8-128 characters, and consist of at least one type of character (uppercase, lowercase, numbers, and symbols). By using a function to get random characters from 4 sets of arrays, we can identify which characters will be filtered into the generated password based on the checkbox elements assigned to each character type. Then we use a "for loop" to fill the new generated password string until it has the desired amount of characters. The application works quickly, and efficiently to produce strong passwords. 
 
+## Useage
+Use to randomly generate secure passwords for frequent password updates to maintain the highest level of security. 
 
 # Resources
 Checkbox - 
@@ -26,4 +29,4 @@ https://developer.mozilla.org/en-US/docs/Web/API/HTMLInputElement
 
 YOUTUBE - Traversy Media
 https://www.youtube.com/watch?v=duNmhKgtcsI
-This instructional video made this assignment possible. I was very stuck on where to start, and how to connect the pieces within the script. I now have a better understanding of how the process flows, and follows the patterns of initial pseudocode.  
+This instructional video made this assignment possible. I was very stuck on where to start, and how to connect the pieces within the script. I now have a better understanding of how the javascript writing process flows, and essentially follows the patterns of initial pseudocoding. 
--- main.js
+++ main.js
@@ -1,6 +1,3 @@
-/*I watched this YouTube video, and followed each step to complete the javascript for this assignment. 
-I wish I didn't have to, but now things make more sense, even though we didn't cover these functions in class.*/
-
 
 //DOM ELEMENTS - Define HTML elements by ID as constant variable objects.
 
@@ -29,9 +26,9 @@ generateEl.addEventListener('click', () => {
 	const hasNumber = numbersEl.checked;
 	const hasSymbol = symbolsEl.checked;
 
+//displays password in the HTML element 
   resultEl.innerHTML = generatePassword(
 	  hasLower, hasUpper, hasNumber, hasSymbol,length);
-
 });
 
 //Generate Password Function - passes random characters into function
@@ -41,22 +38,23 @@ function generatePassword (lower, upper, number, symbol, length) {
 	// make it the set length
 	// put the password in the display box
 
+//defines generatedPassword as a string
 let generatedPassword = '';
 
 //this tells us how many boxes the user checked
 const typesCount = lower + upper + number + symbol;
-	console.log('typesCount: ',typesCount);
+	//console.log('typesCount: ',typesCount);
 
 // by putting curly braces around the array types, it returns more specific info to build a new array
 const typesArr = [{lower}, {upper}, {number}, {symbol}].filter
-//filter is an array method that loops through the items, and adds together the true/checked elements into an array.
+//filter is an array method that loops through the items, and adds together the checked elements into an array.
 (item=> Object.values(item)[0]);
-	console.log('typesArr: ',typesArr)
+	//console.log('typesArr: ',typesArr)
 
 //check if there are no boxes checked, return an alert, and do not generate password
 if(typesCount === 0){
+	alert("Please select one character type.");
 	return '';
-	// alert('Please select at least one character type');
 }
 
 //call the generator function for each type, and 'for' loop it until it meets 
@@ -91,28 +89,36 @@ function getRandomLower () {
 	return String.fromCharCode(Math.floor(Math.random()*26) +97);
 }
 
-console.log(getRandomLower());
+//THIS WORKS!
+//console.log(getRandomLower());
 
 /*charset for uppercase letters a-z are 65-90 - add a random number between 
 1-26, then add that array # to 65 to get into the lowercase range of characters */
 function getRandomUpper () {
 	return String.fromCharCode(Math.floor(Math.random()*26) +65);
 }
-console.log(getRandomUpper());
+//THIS WORKS!
+//console.log(getRandomUpper());
 
 /*charset for numbers 0-9 are 48-57 - add a random number between 
 1-10, then add that array #to 48 to get into the lowercase range of characters */
 function getRandomNumber () {
 	return String.fromCharCode(Math.floor(Math.random()*10) +48);
 }
-console.log(getRandomNumber());
+//THIS WORKS!
+//console.log(getRandomNumber());
 
 /* define the string of symbols used in generator, return a random character from that string */
 function getRandomSymbol () {
 	const symbols = '!@#$%^&*?~';
 	return symbols[Math.floor(Math.random()* symbols.length)];
 }
-console.log(getRandomSymbol());
-//=================================================================================
+
+//THIS WORKS!
+//console.log(getRandomSymbol());
+
+
+
+//yay
 
 
